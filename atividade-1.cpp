bcd326f2c5da172c4f67fb2f8d011f244187a90e
mateus da silva santos <mateusrjm4@gmail.com>
2023-02-28 12:58:28 -0300

    Adicionar arquivos de projeto.

---
+++ atividade-1.cpp
@@ -0,0 +1,43 @@
+// atividade-1.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Calculo de media" << endl;
+	cout << "================" << endl;
+
+	string nome = "";
+	double notaP1 = 0;
+	double notaP2 = 0;
+	double media = 0;
+
+	cout << "Digite seu nome" << endl;
+	cin >> nome;
+
+	cout << "Digite a nota da P1" << endl;
+	cin >> notaP1;
+	cout << "digite a nota da P2 " << endl;
+	cin >> notaP2;
+
+	media = (notaP1 + notaP2) / 2;
+	if (media < 6.0) {
+		cout << "vc esta Reprovado";
+	}
+	else {
+		cout << "vc esta Aprovado";
+	}
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
